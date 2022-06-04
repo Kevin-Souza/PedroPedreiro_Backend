174033efcaef5bfc8d20553955a4d10e129de6a1
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-06-04 14:56:31 -0300

    new base

---
+++ src/main/java/br/com/pedropedreiro/pedropedreiro_api/PedropedreiroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedropedreiro.pedropedreiro_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedropedreiroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedropedreiroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedropedreiro/pedropedreiro_api/PedropedreiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedropedreiro.pedropedreiro_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedropedreiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
