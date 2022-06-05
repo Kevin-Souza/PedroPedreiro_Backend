4215ceb84eb2de50dffc60a610913934547b8eae
DW Rodrigues <denys.w.r@hotmail.com>
2022-06-05 16:41:02 -0300

    Ajustes

--- src/test/java/br/com/pedropedreiro/pedropedreiro_api/PedropedreiroApiApplicationTests.java
+++ src/test/java/br/com/pedropedreiro/pedropedreiro_api/PedropedreiroApiApplicationTests.java
@@ -1,13 +1,14 @@
 package br.com.pedropedreiro.pedropedreiro_api;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PedropedreiroApiApplicationTests {
+public class PedropedreiroApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	
 }
